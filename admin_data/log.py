8ede447ef2dbe9d6586324383b6195addfdb1594
RPPython <pr79@abv.bg>
2024-04-16 17:01:58 +0300

    log_admin

--- admin_data/log.py
+++ admin_data/log.py
@@ -1,12 +1,28 @@
 # Function to authenticate the administrator
+def main():
+    if admin_login():
+        print("\nHello! Welcome, to the user managment system!")
+    else:
+        print("\nLogin failed. Please check the entered data.")
+    
 
 def admin_login():
     print("Administrator Login")
     name = input("Enter your name: ")
     dob = input("Enter your date of birth (YYYY-MM-DD): ")
     address = input("Enter your address: ")
-    
-    return True # Only valid for demonstration proposes
 
-# I can customize the admin login to the system, add job title for example
+if __name__ == "__main__":
+    main()
+
+
+    
+    # return True ### Only valid for demonstration proposes
+
+# I can customize the admin login to the system, add job title for example 
+# and does the user have the necessary credentials to log in as
    
+    
+    
+
+ 
