2a6f070676346820d21d9b1e3da8185c50aab865
RPPython <pr79@abv.bg>
2024-04-16 17:16:37 +0300

    coment

--- admin_data/log.py
+++ admin_data/log.py
@@ -1,4 +1,4 @@
-# Function to authenticate the administrator
+# Start main program
 def main():
     if admin_login():
         print("\nHello! Welcome, to the user managment system!")
@@ -6,11 +6,13 @@ def main():
         print("\nLogin failed. Please check the entered data.")
     
 
+# Function to authenticate the administrator
+
 def admin_login():
     print("Administrator Login")
-    name = input("Enter your name: ")
-    dob = input("Enter your date of birth (YYYY-MM-DD): ")
-    address = input("Enter your address: ")
+    name = input("Enter your name: ") ### adding a dictionary with keys for first name, last name, last name
+    dob = input("Enter your date of birth (YYYY-MM-DD): ") 
+    address = input("Enter your address: ") ### add dict for city, str., nomber, ZIP code
 
     # return True ### Only valid for demonstration proposes
 
@@ -20,6 +22,7 @@ if __name__ == "__main__":
 
 
 # I can customize the admin login to the system, add job title for example 
+# for date of birth I can adding a button with a drop-down menu to select date, month, year
 # and does the user have the necessary credentials to log in as adminstrator
    
     
