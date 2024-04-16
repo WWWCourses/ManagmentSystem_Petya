f1633662e6c75d06e6307a8cbaaa30c21f6fa197
RPPython <pr79@abv.bg>
2024-04-16 16:44:10 +0300

    strart with project condition

---
+++ admin_data/log.py
@@ -0,0 +1,12 @@
+# Function to authenticate the administrator
+
+def admin_login():
+    print("Administrator Login")
+    name = input("Enter your name: ")
+    dob = input("Enter your date of birth (YYYY-MM-DD): ")
+    address = input("Enter your address: ")
+    
+    return True # Only valid for demonstration proposes
+
+# I can customize the admin login to the system, add job title for example
+   
